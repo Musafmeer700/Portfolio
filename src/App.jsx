30bceb348bc9dbbe79d8ad650252a75937d5835c
MusafMmeer700 <abdvilliers077@gmail.com>
2026-03-23 16:34:31 +0500

    implement project section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar"
 import HeroSection from './components/HeroSection';
 import About from './components/About';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
 
 const App = () => {
 
@@ -40,6 +41,7 @@ const App = () => {
       <HeroSection darkMode={darkMode} />
       <About darkMode={darkMode} />
       <Skills darkMode={darkMode} />
+      <Projects darkMode={darkMode} />
     </div>
   )
 }
